014d18039db7e5a51a8f75b9b16e4e056c0a79f9
Dharmanand <kumar.kd20@gmail.com>
2020-06-12 08:48:19 +0530

    first commit

---
+++ src/main/java/com/kd/reg/RegistrationLoginLogoutSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kd.reg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginLogoutSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginLogoutSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kd/reg/RegistrationLoginLogoutSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginLogoutSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
