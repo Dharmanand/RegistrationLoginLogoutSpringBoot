ae741952213736f435959403d2baf329da2af79f
Dharmanand <kumar.kd20@gmail.com>
2020-06-12 09:20:23 +0530

    second commit

--- src/main/java/com/kd/reg/RegistrationLoginLogoutSpringBootApplication.java
+++ src/main/java/com/kd/reg/RegistrationLoginLogoutSpringBootApplication.java
@@ -2,7 +2,7 @@ package com.kd.reg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class RegistrationLoginLogoutSpringBootApplication {
 
